b6f70039cbb743f1a5542faf435f768c4a32db1e
SammCheese <sammcheese.github@gmail.com>
2022-01-13 02:56:39 +0100

    Fix Updater once More

--- index.js
+++ index.js
@@ -5,55 +5,53 @@ const path = require('path');
 
 const cryptPath = path.join(__dirname, 'components', 'SimpleDiscordCrypt.user.js');
 
-const download = function(url, path) {
-  var file = fs.createWriteStream(path);
-  var request = https.get(url, function(response) {
+const download = function (url, path) {
+  const file = fs.createWriteStream(path);
+  const request = https.get(url, (response) => {
     response.pipe(file);
-    file.on('finish', function() {
+    file.on('finish', () => {
       file.close();
-    })
+    });
   });
 };
 
-function hasUpdates(input, input2) {
-  var version;
-
-
+async function hasUpdates (input, input2) {
   if (!fs.existsSync(input)) return true;
 
   https.get(input2, (res) => {
     res.on('data', (d) => {
       if (d.toString('utf-8').includes('// @version')) {
-        version = d.toString('utf-8').split('// @version')[1].split('\n')[0];
-        version = parseInt(version.split('.').join(""));
+        let version = d.toString('utf-8').split('// @version')[1].split('\n')[0];
+        version = parseInt(version.split('.').join(''));
+        fs.readFile(input, 'utf8', (err, data) => {
+          if (err) return true;
+          if (data.includes('// @version')) {
+            let currentVersion = data.split('// @version')[1].split('\n')[0];
+            currentVersion = parseInt(currentVersion.split('.').join(''));
+            if (currentVersion < version) {
+              console.log(`%c[SimpleDiscordCrypt]%c Update available: ${currentVersion} -> ${version}\n Downloading now...`, 'color: #4CFFCF; font-weight: bold;', 'color: #ffffff;');
+              download('https://gitlab.com/An0/SimpleDiscordCrypt/raw/master/SimpleDiscordCrypt.user.js', cryptPath);
+            }
+          }
+        });
       }
-      else return false;
     }).on('error', (e) => {
       console.error(e);
       return false;
     });
   });
-
-  fs.readFile(input, 'utf8', (err, data) => {
-    if (err) return true;
-    if (data.includes('// @version')) {
-      let currentVersion = data.split('// @version')[1].split('\n')[0];
-      currentVersion = parseInt(currentVersion.split('.').join(""));
-      return version > currentVersion;
-    }
-  });
 }
 
 module.exports = class SimpleDiscordCrypt extends Plugin {
-  startPlugin() {
-    if (hasUpdates(cryptPath, 'https://gitlab.com/An0/SimpleDiscordCrypt/raw/master/SimpleDiscordCrypt.meta.js')) {
-      console.log('Updating SimpleDiscordCrypt');
-      download('https://gitlab.com/An0/SimpleDiscordCrypt/raw/master/SimpleDiscordCrypt.user.js', cryptPath);
-    }
-    fs.readFile(cryptPath, 'utf8', (err, data) => {
-      if (err) return;
-      eval(data);
-    });
+  async startPlugin () {
+    await hasUpdates(cryptPath, 'https://gitlab.com/An0/SimpleDiscordCrypt/raw/master/SimpleDiscordCrypt.meta.js').then(
+      () => {
+        fs.readFile(cryptPath, 'utf8', (err, data) => {
+          if (err) return;
+          eval(data);
+        });
+      }
+    )
   }
 
   pluginWillUnload() {
